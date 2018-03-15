2b021624350993f750eb245cd6a30012bbd4bd0a
clwaddel <30387984+clwaddel@users.noreply.github.com>
2018-03-14 20:29:24 -0400

    'initial'

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -s /usr/local/cuda-9.1/lib64/libcudart.so /usr/lib/libcudart.so
 ln -s /usr/local/cuda-9.1/lib64/libcudart.so.9.1 /usr/lib/libcudart.so.9.1
 apt-get install --yes libjansson-dev
+git clone https://github.com/peters/curl-for-windows.git compat/curl-for-windows
 mv install.sh install-mm.sh
 
 # Simple script to create the Makefile and build
